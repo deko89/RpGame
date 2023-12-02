3663ad136db1e4c97f42699322ab44785bacc4d5
deko89 <dects@yandex.ru>
2023-12-02 15:15:37 +0300

    Demo.

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,26 @@ int main()
 	if (eng.Init() == false)
 		return -1;
 
+    // Настройка.
+    View* view = eng.CreateViewWorld();
+    view->shaders.posTex.Use();
+    // Установка камеры.
+    view->cam.pos = {0, -3, 1};
+    view->cam.angle = {0, 0, pi/2};
+    view->cam.Update();
+    // Создание текстуры.
+    static Texture tex;
+    tex.Load("SkyBox/0.jpg");
+    tex.Use();
+    // Создание модели.
+    Mesh mesh;
+    mesh.MakeCylinder(0.3, 2, 8, 1);
+    PlaceTex plTex;
+    plTex.SetCylinder(8, 1, 2, 1);
+    ModelVi* mod = new ModelVi;
+    mod->Make(mesh, plTex);
+    view->world->models.Add(mod);
+
 	eng.Execute();
 
 	return 0;
