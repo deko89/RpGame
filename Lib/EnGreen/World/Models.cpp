769102146c4e6aaada7ac8a82ac8d2f8b5f597b9
deko89 <dects@yandex.ru>
2024-03-16 21:03:18 +0300

    ModelStd

--- Lib/EnGreen/World/Models.cpp
+++ Lib/EnGreen/World/Models.cpp
@@ -3,6 +3,18 @@
 namespace EnG
 {
 
+// ModelStd /////////////////////////////////////////////////////////
+ModelStd::ModelStd(ShaderType shT, Texture& tex) :
+	shader(shT),
+	texture(tex)
+{
+}
+void ModelStd::Draw() const
+{
+	shader.Use();
+	texture.Use();
+	ModelVi::Draw();
+}
 // BaseEngine ///////////////////////////////////////////////////////
 void Models::Draw() const
 {   for (Model* m : *this)
--- Lib/EnGreen/World/Models.h
+++ Lib/EnGreen/World/Models.h
@@ -2,10 +2,21 @@
 #define Models_H
 
 #include "Std/MassivPtr.h"
+#include "EnGreen/Base/Meta/Texture.h"
 #include "EnGreen/Base/Meta/Model.h"
 
 namespace EnG
 {
+
+/// Стандартная модель.
+class ModelStd: public ModelVi
+{	public:
+	ModelStd(ShaderType shT, Texture& tex);
+	virtual void Draw() const override;
+protected:
+	ShaderRef shader;
+	TextureRef texture;
+};
 
 /// Модели.
 class Models : public MassivPtr<Model>
