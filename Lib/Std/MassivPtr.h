e450b9b1067381d2305d961df20fc24a5d3722c1
deko89 <dects@yandex.ru>
2024-04-29 20:57:39 +0300

    MassivPtr::clear

--- Lib/Std/MassivPtr.h
+++ Lib/Std/MassivPtr.h
@@ -7,10 +7,16 @@
 template<class C> class MassivPtr: public std::vector<C*>
 {   public:
     typedef std::vector<C*> Base;
-	~MassivPtr() { for (C* e: *this) delete e; }
+	~MassivPtr() { clear(); }
 	C* Add() { C* p = new C;   Base::push_back(p);   return p; }
 	C* Add(C* p) { Base::push_back(p);   return p; }
 	template<typename T = C, typename... Arg> T* Make(Arg... arg) { T* p = new T(arg...);   Base::push_back(p);   return p; }
+	void clear()
+	{
+		for (C* e: *this) delete e;
+		Base::clear();
+	}
+
 	MassivPtr& operator= (const MassivPtr& B) = delete;
 	typename Base::iterator erase(typename Base::const_iterator i) = delete;
 	typename Base::iterator erase(typename Base::const_iterator first, typename Base::const_iterator last) = delete;
